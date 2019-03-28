8c4fad47b73ca1b88178581f32cafd376a04bcf1
totl41 <naumovbox@gmail.com>
2019-03-28 15:32:03 +0300

    Решенные задачи 1 уровня

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0126/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task01.task0126;
+
+/* 
+Не хочешь  - решай нехотя
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String a = "Хочешь - решай задачи, не хочешь - решай нехотя";
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        //напишите тут ваш код
+        System.out.println(" ");
+        System.out.println("111111111");
+        System.out.println("222222222");
+        System.out.println("333333333");
+        System.out.println("444444444");
+    }
+}
