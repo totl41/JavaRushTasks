29e2358a098cf26ad6e71aecd817734f86d2d2c7
totl41 <naumovbox@gmail.com>
2019-03-28 15:13:51 +0300

    Создание хранилища

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task01.task0101;
+
+/* 
+Я думаю, быть программистом - это круто
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.print("Я ");
+        System.out.print("думаю, ");
+        System.out.print("быть ");
+        System.out.print("программистом ");
+        System.out.print("- это ");
+        System.out.print("круто");
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0102/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task01.task0102;
+
+/* 
+Комплимент учителю
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String StringOut = "Элли самая красивая";
+        System.out.println(StringOut);//1
+        System.out.println(StringOut);//2
+        System.out.println(StringOut);//3
+        System.out.println(StringOut);//4
+        System.out.println(StringOut);//5
+    }
+}
