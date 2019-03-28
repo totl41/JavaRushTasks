df1d7a8f498d27a25a64f6a56765d0bd9373ccb4
totl41 <naumovbox@gmail.com>
2019-03-28 15:22:07 +0300

    Решенные задачи 0 уровня

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0105;
+
+/* 
+Объявляем переменные
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String name, city;
+        int age;
+    }
+}
