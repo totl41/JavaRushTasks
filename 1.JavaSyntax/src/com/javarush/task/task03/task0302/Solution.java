195acc30b10bcaca744e0bda53ae3154a11cd200
totl41 <naumovbox@gmail.com>
2019-04-07 16:40:44 +0300

    Уровнь 3. Решена задача 0302 "Немедленно в печать"

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0302/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task03.task0302;
+
+import sun.awt.geom.AreaOp;
+
+/*
+Немедленно в печать
+*/
+public class Solution {
+    // напишите тут ваш код
+    public static void printString(String S){
+        System.out.println(S);
+    }
+
+    public static void main(String[] args) {
+        printString("Hello, Amigo!");
+    }
+}
