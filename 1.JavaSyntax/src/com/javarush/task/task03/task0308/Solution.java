46ae56c57f884bad4d9f34884aafba3893c5e55c
totl41 <naumovbox@gmail.com>
2019-04-15 16:35:42 +0300

    Уровнь 3. Решена задача 0308 "Произведение 10 чисел"

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task03.task0308;
+
+/* 
+Произведение 10 чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int x = 1;
+
+        for (int i = 1; i < 11; i++) {
+
+            x = x * i;
+
+        }
+
+        System.out.println(x);
+
+    }
+}
