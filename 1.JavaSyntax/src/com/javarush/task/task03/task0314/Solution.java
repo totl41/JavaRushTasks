9864119c2a77724b204f5a1081e7add13388f2b3
totl41 <naumovbox@gmail.com>
2019-04-15 21:17:53 +0300

    Уровнь 3. Решена задача 0314 "Таблица умножения"

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task03.task0314;
+
+/* 
+Таблица умножения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        for (int y = 1; y < 11; y++){
+
+            for (int x = 1; x < 11; x++){
+
+                System.out.print((x * y) + " ");
+
+            }
+          System.out.println();
+
+        }
+    }
+}
