359e7c488abf80a303523606913cfc664555e573
totl41 <naumovbox@gmail.com>
2019-04-15 16:47:44 +0300

    Уровнь 3. Решена задача 0309 "Сумма 5 чисел"

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0309/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task03.task0309;
+
+/* 
+Сумма 5 чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int x = 0;
+
+        for (int i = 1; i < 6; i++){
+
+            x = x + i;
+
+            System.out.println(x);
+
+        }
+
+    }
+}
