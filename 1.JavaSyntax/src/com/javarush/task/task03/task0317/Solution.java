c3a80a38422653eac8a6f520622be45da1499f8d
totl41 <naumovbox@gmail.com>
2019-04-18 17:44:44 +0300

    Уровнь 3. Решена задача 0317 "Путь самурая"

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0317/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task03.task0317;
+
+/* 
+Путь самурая
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        char a = '\u65e5';
+        char b = '\u672c';
+        char c = '\u8a9e';
+
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+    }
+}
