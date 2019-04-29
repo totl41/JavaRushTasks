6e53877297551cf013a84fe2903382f063e0cb1b
totl41 <naumovbox@gmail.com>
2019-04-29 15:44:25 +0300

    Уровнь 4. Условие задачи 0416 "Переходим дорогу вслепую"

--- 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
@@ -9,5 +9,23 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        double timeReader = Double.valueOf(reader.readLine());
+        String color = null;
+
+        double timeTracker = timeReader % 5;
+
+        if (timeTracker <= 0.0 || timeTracker < 3.0){
+            color = "зелёный";
+        }
+        else if (timeTracker >= 3.0 & timeTracker < 4.0){
+            color = "жёлтый";
+        }
+        else if (timeTracker >= 4.0 & timeTracker < 5.0){
+            color = "красный";
+        }
+
+        System.out.println(color);
     }
 }
