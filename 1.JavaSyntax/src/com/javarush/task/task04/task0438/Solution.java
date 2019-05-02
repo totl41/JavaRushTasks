91f484246539bc3522b791eae94482a2159fbc58
totl41 <naumovbox@gmail.com>
2019-05-02 23:17:12 +0300

    Уровнь 4. В процессе решения задача 0438 "Рисуем линии"

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task04.task0438;
+
+/* 
+Рисуем линии
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        for (int i = 1; i <= 9; i++){
+            System.out.print("8");
+        }
+        System.out.println("8");
+        for (int i = 1; i <= 10; i++){
+            System.out.println("8");
+        }
+    }
+}
