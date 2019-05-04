abe4054e19e8c25f5f6ca2d06c184ffd3ef0d1de
totl41 <naumovbox@gmail.com>
2019-05-04 22:34:50 +0300

    Уровнь 5. Решения задача 0504 "Трикотаж"

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0504/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task05.task0504;
+
+
+/* 
+Трикотаж
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat cat1 = new Cat("Барсик", 3, 5, 5);
+        Cat cat2 = new Cat("Васька", 4, 4, 4);
+        Cat cat3 = new Cat("Тоська", 7, 3, 3);
+
+    }
+
+    public static class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+    }
+}
