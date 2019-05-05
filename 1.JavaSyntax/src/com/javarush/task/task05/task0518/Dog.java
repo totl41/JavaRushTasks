e9a16b77aa48708593e06d1774077282e0e73b99
totl41 <naumovbox@gmail.com>
2019-05-05 22:29:16 +0300

    Уровнь 5. Решения задача 0518 "Регистрируем собачек"

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0518/Dog.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task05.task0518;
+
+/* 
+Регистрируем собачек
+*/
+
+
+public class Dog {
+    //напишите тут ваш код
+    private String name;
+    private int height;
+    private String color;
+
+    //1
+    public Dog(String name){
+        this.name = name;
+    }
+
+    //2
+    public Dog(String name, int height){
+        this.name = name;
+        this.height = height;
+    }
+
+    //3
+    public Dog(String name, int height, String color){
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
