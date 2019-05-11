c4e7403a5cdd3b7330d8e3d9948a617c32320fcc
totl41 <naumovbox@gmail.com>
2019-05-11 16:52:21 +0300

    Уровнь 6. Решения задача 0606 "Чётные и нечётные циферки"

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0606/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task06.task0606;
+
+import java.io.*;
+
+/* 
+Чётные и нечётные циферки
+*/
+
+public class Solution {
+
+    public static int even;
+    public static int odd;
+
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String stringIn = reader.readLine();
+        int numberIn = 0;
+
+        for (int i = 0; i < stringIn.length(); i++) {
+
+            numberIn = Character.getNumericValue(stringIn.charAt(i));
+
+            if (numberIn % 2 == 0) {
+                even ++;
+            }
+            else {
+                odd ++;
+            }
+        }
+        System.out.println("Even: " + even + " Odd: " + odd);
+    }
+}
