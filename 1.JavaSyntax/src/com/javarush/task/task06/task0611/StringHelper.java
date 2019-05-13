83d41c7f8142958d4b8be6a991e679a63d9500b5
totl41 <naumovbox@gmail.com>
2019-05-13 18:00:23 +0300

    Уровнь 6. Условие задачи 0611 "Класс StringHelper"

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0611/StringHelper.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task06.task0611;
+
+/* 
+Класс StringHelper
+*/
+
+public class StringHelper {
+    public static String multiply(String s) {
+        String result = "";
+
+        //напишите тут ваш код
+        for (int i = 1; i <= 5 ; i++) {
+            result = result + s;
+        }
+
+        return result;
+    }
+
+    public static String multiply(String s, int count) {
+        String result = "";
+
+        //напишите тут ваш код
+        for (int i = 1; i <= count; i++) {
+            result = result + s;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
