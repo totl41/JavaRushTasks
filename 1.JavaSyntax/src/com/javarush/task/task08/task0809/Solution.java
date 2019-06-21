395fb794f8250db5e840dfee300d2d4ad825af6e
totl41 <naumovbox@gmail.com>
2019-06-21 22:01:19 +0400

    Уровнь 8. Решения задача 0809 "Время для 10 тысяч вставок"

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0809/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task08.task0809;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Время для 10 тысяч вставок
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getInsertTimeInMs(new ArrayList()));
+        System.out.println(getInsertTimeInMs(new LinkedList()));
+    }
+
+    public static long getInsertTimeInMs(List list) {
+        // напишите тут ваш код
+        Date dateStart = new Date();
+
+        insert10000(list);
+
+        // напишите тут ваш код
+        Date dateFinish = new Date();
+        long leadTime = dateFinish.getTime() - dateStart.getTime();
+
+        return leadTime;
+    }
+
+    public static void insert10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.add(0, new Object());
+        }
+    }
+}
