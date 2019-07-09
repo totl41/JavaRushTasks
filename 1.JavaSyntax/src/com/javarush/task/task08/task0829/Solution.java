3cd603c40e94ab293cde3251cb625b65f108a9d1
totl41 <naumovbox@gmail.com>
2019-07-09 17:51:27 +0300

    Уровнь 8. Условие задачи 0829 "Модернизация ПО"

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task08.task0829;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Модернизация ПО
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<String> list = new ArrayList<>();
+        while (true) {
+            String family = reader.readLine();
+            if (family.isEmpty()) {
+                break;
+            }
+            list.add(family);
+        }
+
+        // Read the house number
+        //int houseNumber = Integer.parseInt(reader.readLine());
+
+//        if (0 <= houseNumber && houseNumber < list.size()) {
+//            String familyName = list.get(houseNumber);
+//            System.out.println(familyName);
+//        }
+        String coutry = reader.readLine();
+        int index = list.lastIndexOf(coutry);
+        if (index != -1) {
+            System.out.println(list.get(index + 1));
+        }
+    }
+}
