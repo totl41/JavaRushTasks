14b61208e1c1c1624ae70bec0e442e7e57fdaf8f
totl41 <naumovbox@gmail.com>
2019-07-13 18:40:32 +0300

    Уровнь 9. Решение задача 0910 "Исключение при работе с коллекциями List"

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0910/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task09.task0910;
+
+import jdk.nashorn.internal.runtime.arrays.ArrayIndex;
+
+import java.util.ArrayList;
+
+/* 
+Исключение при работе с коллекциями List
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        try{
+            ArrayList<String> list = new ArrayList<String>();
+            String s = list.get(18);
+        }
+        //напишите тут ваш код
+        catch(IndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+    }
+}
