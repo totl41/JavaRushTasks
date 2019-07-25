813960a6b5c57ca02e4e32fed8ea7003ac01c4ee
totl41 <naumovbox@gmail.com>
2019-07-25 14:01:24 +0300

    Уровнь 9. Условие задачи 0929 "Обогатим код функциональностью!"

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0929/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task09.task0929;
+
+import java.io.*;
+
+/* 
+Обогатим код функциональностью!
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        InputStream fileInputStream = null;
+        String sourceFileName = "";
+
+        for (int i = 0; i < 2; i++) {
+            sourceFileName = reader.readLine();
+            try {
+                fileInputStream = getInputStream(sourceFileName);
+                i = 2;
+            }
+            catch (IOException e){
+                System.out.println("Файл не существует.");
+            }
+        }
+
+        String destinationFileName = reader.readLine();
+        OutputStream fileOutputStream = getOutputStream(destinationFileName);
+
+        while (fileInputStream.available() > 0) {
+            int data = fileInputStream.read();
+            fileOutputStream.write(data);
+        }
+
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+
+    public static InputStream getInputStream(String fileName) throws IOException {
+        return new FileInputStream(fileName);
+    }
+
+    public static OutputStream getOutputStream(String fileName) throws IOException {
+        return new FileOutputStream(fileName);
+    }
+}
+
