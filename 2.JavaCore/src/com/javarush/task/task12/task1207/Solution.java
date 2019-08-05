d014791b63edaf8de1ea568716ce479969be1f5a
totl41 <naumovbox@gmail.com>
2019-08-06 00:49:48 +0400

    Уровнь 12. Задача 1207 "Int и Integer"

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = 0;
+        Integer y = 0;
+        print(x);
+        print(y);
+    }
+
+    //Напишите тут ваши методы
+    static void print(int x){
+    }
+
+    static void print(Integer x){
+    }
+}
