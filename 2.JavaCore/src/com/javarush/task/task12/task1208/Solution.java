9541c9e7406d12436eb3d7546a092ae0bb0ca59f
totl41 <naumovbox@gmail.com>
2019-08-06 00:55:57 +0400

    Уровнь 12. Задача 1208 "Свобода печати"

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    static void print(int x){
+    }
+
+    static void print(String x){
+    }
+
+    static void print(char x){
+    }
+
+    static void print(boolean x){
+    }
+
+    static void print(long x){
+    }
+}
