51cf58cb917850be5adbc7451189aa6f7fd094a8
totl41 <naumovbox@gmail.com>
2019-08-07 22:08:30 +0400

    Уровнь 12. Задача 1209 "Три метода и максимум"

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static int max(int x, int y){
+        if (x < y) return y;
+        else return x;
+    }
+
+    public static long max(long x, long y){
+        if (x < y) return y;
+        else return x;
+    }
+
+    public static double max(double x, double y){
+        if (x < y) return y;
+        else return x;
+    }
+}
