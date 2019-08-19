3b8522b1895e65d6d4329fb7338578b152545396
totl41 <naumovbox@gmail.com>
2019-08-19 17:59:18 +0300

    Уровнь 12. Задача 1224 "Неведома зверушка"

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1224/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task12.task1224;
+
+/* 
+Неведома зверушка
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getObjectType(new Cat()));
+        System.out.println(getObjectType(new Tiger()));
+        System.out.println(getObjectType(new Lion()));
+        System.out.println(getObjectType(new Bull()));
+        System.out.println(getObjectType(new Pig()));
+    }
+
+    public static String getObjectType(Object o) {
+        //напишите тут ваш код
+        String animals = "Животное";
+
+        if (o instanceof Cat) animals = "Кот";
+        else if (o instanceof Tiger) animals = "Тигр";
+        else if (o instanceof Lion) animals = "Лев";
+        else if (o instanceof Bull) animals = "Бык";
+
+        return animals;
+    }
+
+    public static class Cat {
+    }
+
+    public static class Tiger {
+    }
+
+    public static class Lion {
+    }
+
+    public static class Bull {
+    }
+
+    public static class Pig {
+    }
+}
