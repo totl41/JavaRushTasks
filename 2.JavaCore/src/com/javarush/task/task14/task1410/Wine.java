92b268db1208e84cdf292aacb9eda4f4594c87f9
totl41 <naumovbox@gmail.com>
2019-09-19 17:45:33 +0300

    Уровнь 14. Задача 1410 "Дегустация вин"

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/Wine.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task14.task1410;
+
+import com.javarush.task.task14.task1410.Drink;
+
+public class Wine extends Drink {
+
+    public String getHolidayName(){
+        return "День Рождения";
+    }
+}
