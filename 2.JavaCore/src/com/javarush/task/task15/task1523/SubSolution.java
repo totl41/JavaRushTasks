83c2495a0d2c6be1cc49ae9b66dd90491fe31e9c
totl41 <naumovbox@gmail.com>
2019-12-11 20:22:40 +0300

    Уровнь 15. Задача 1523 "Перегрузка конструкторов"

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+
+    protected SubSolution(Integer s) {
+        super(s);
+    }
+
+    SubSolution(Short s) {
+        super(s);
+    }
+
+    public SubSolution(Long s) {
+        super(s);
+    }
+}
